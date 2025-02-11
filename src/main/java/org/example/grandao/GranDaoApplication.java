bd667ce6bd4cbb9738392c21beb82fac2c4b2e89
moraranda <moraranda.e@gmail.com>
2025-02-11 08:31:57 +0100

    App

---
+++ src/main/java/org/example/grandao/GranDaoApplication.java
@@ -0,0 +1,13 @@
+package org.example.grandao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GranDaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/grandao/GranDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grandao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
