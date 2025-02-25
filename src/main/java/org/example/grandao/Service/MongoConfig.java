5614dfd9316d59da8999d043a059576507b11773
AliciaPM08 <apachecomena@gmail.com>
2025-02-25 08:36:59 +0100

    Corregido el error mongo

---
+++ src/main/java/org/example/grandao/Service/MongoConfig.java
@@ -0,0 +1,16 @@
+package org.example.grandao.Service;
+
+import com.mongodb.client.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, "biblioteca");
+    }
+}
+
