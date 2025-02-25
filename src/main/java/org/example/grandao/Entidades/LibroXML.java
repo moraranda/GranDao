565c4ed31eb83d213f609983bcf5ec2ac1bc9db0
AliciaPM08 <apachecomena@gmail.com>
2025-02-25 22:31:58 +0100

    Hecho los XML de libro

---
+++ src/main/java/org/example/grandao/Entidades/LibroXML.java
@@ -0,0 +1,49 @@
+package org.example.grandao.Entidades;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement(name = "libro")
+public class LibroXML implements Serializable {
+
+    private String isbn;
+    private String titulo;
+    private String autor;
+
+    public LibroXML() {
+    }
+
+    public LibroXML(String isbn, String titulo, String autor) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    @XmlElement(name = "isbn")
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @XmlElement(name = "titulo")
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @XmlElement(name = "autor")
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
