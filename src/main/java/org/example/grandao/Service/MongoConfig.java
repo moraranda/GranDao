de5b94cc29244064b153bb1db568a89401f47c80
AliciaPM08 <apachecomena@gmail.com>
2025-02-26 13:10:45 +0100

    Comentarios service

--- src/main/java/org/example/grandao/Service/MongoConfig.java
+++ src/main/java/org/example/grandao/Service/MongoConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+
+//Configuracion para que la parte de Mongo funcione
 @Configuration
 public class MongoConfig {
 
+    //Conecta con la bd biblioteca
     @Bean
     public MongoTemplate mongoTemplate(MongoClient mongoClient) {
         return new MongoTemplate(mongoClient, "biblioteca");
